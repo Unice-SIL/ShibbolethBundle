b832792bedda6ffc332e828cf45acf4568b94393
Fred <frederic.casazza@univ-cotedazur.fr>
2022-02-11 13:47:42 +0100

    refactor: adapt authenticator and provider to sf 5.4

---
+++ Security/Provider/AbstractShibbolethUserProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace UniceSIL\ShibbolethBundle\Security\Provider;
+
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use UniceSIL\ShibbolethBundle\Security\ShibbolethAuthenticator;
+
+abstract class AbstractShibbolethUserProvider implements UserProviderInterface
+{
+    /**
+     * @var SessionInterface
+     */
+    private $session;
+
+    /**
+     * @param RequestStack $requestStack
+     */
+    public function __construct(RequestStack $requestStack)
+    {
+        $this->session = $requestStack->getSession();
+    }
+
+    /**
+     * @return array
+     */
+    private function getAttributes(): array {
+        return $this->session->get(ShibbolethAuthenticator::SESSION_SHIBBOLETH_USER_ATTRIBUTES, []);
+    }
+}
